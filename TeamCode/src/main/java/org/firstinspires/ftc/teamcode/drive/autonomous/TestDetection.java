4c65d7f4a12ff65527e8ae895675400abfb3afe8
mihnwq <86513396+mihnwq@users.noreply.github.com>
2021-12-12 19:16:30 +0200

    Added detection
    Adaugat toate librariile necesare si creat un cod simplu de detectare a cuburilor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/autonomous/TestDetection.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.drive.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//Program temporar
+public class TestDetection extends LinearOpMode {
+
+    BlockDetection bloc = new BlockDetection();
+
+    public int caz;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        bloc.init(hardwareMap);
+
+        while (!isStarted()) {
+            if (bloc.Left_percent < 85 && bloc.Right_percent < 85) {
+                telemetry.addData("Cazul", 4);
+                caz = 4;
+            }
+            if (bloc.Left_percent <85 && bloc.Right_percent > 85) {
+                telemetry.addData("Cazul", 1);
+                caz = 1;
+            }
+            if (bloc.Left_percent > 85 && bloc.Right_percent > 85) {
+                telemetry.addData("Cazul", 0);
+                caz = 0;
+            }
+            telemetry.addData("Big percentage", bloc.Left_percent);
+            telemetry.addData("Small percentage", bloc.Right_percent);
+            telemetry.update();
+        }
+
+    }
+}
